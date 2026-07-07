81cacbb8162fc01124f7887790392dcde8231883
rkbytewave-tech <rkbytewave@gmail.com>
2026-07-07 18:10:22 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
 MODEL_NAME     = "gemini-2.5-flash"
+LITE_MODEL_NAME = "gemini-2.5-flash-lite"
 IMAGE_EXT      = {".png", ".jpg", ".jpeg", ".tiff", ".bmp", ".webp"}
